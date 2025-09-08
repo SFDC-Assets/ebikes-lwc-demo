48fb4d9ce56517ac677d39e4803f4e02c6b0ace4
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2025-09-08 10:32:40 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 30 --wait 60 -s -p maps -e work.shop
 
-sf project deploy start -d trial-app
+# sf project deploy start -d trial-app
 sf demoutil user password set -p salesforce1 -g User -l User
 
 sf org open -p /lightning/setup/SetupOneHome/home
